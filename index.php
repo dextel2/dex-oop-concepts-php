85de0650739e72174d4f1b17213c6c96b3e71c4d
dex.papa@gmail.com <dex.papa@gmail.com>
2018-01-01 02:11:42 +0530

    Added Style and header
    Added  tag and style to Fullname and added blue color to Emal

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
     include 'Student.php';
-    
+
     $mypdo = new MyPDO('mysql:host=localhost;dbname=admission2018;charset=utf8');
     $student = new Student($mypdo);
     $list = $student->findAll();
 
     echo "<b> Displaying All Records </b> <br> <br>";
     foreach ($list as $row) {
-        echo $row['FULLNAME'];
+        echo "<b>Fullname </b>".$row['FULLNAME'];
         echo "<br/>";
-        echo $row['EMAIL'];    
+        echo "<b style='color:blue'>Email ID </b>" . $row['EMAIL'];    
         echo "<br/>";
     }
